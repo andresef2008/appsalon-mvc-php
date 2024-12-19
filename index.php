933ad48f85c1cc1c62b6069dec1503a94b761d04
Andres Figueroa <andresfiguef@gmail.com>
2024-12-18 21:55:26 -0500

    Routing para servicios

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use Controllers\AdminController;
 use Controllers\APIController;
 use Controllers\CitaController;
 use Controllers\loginController;
-
+use Controllers\ServicioController;
 use MVC\Router;
 
 $router = new Router();
@@ -35,6 +35,17 @@ $router->get('/admin', [AdminController::class, 'index']);
 $router->get('/api/servicios', [APIController::class, 'index']);
 $router->post('/api/citas', [APIController::class, 'guardar']);
 $router->post('/api/eliminar', [APIController::class, 'eliminar']);
+
+//CRUD PARA SERVICIOS.
+
+$router->get('/servicios', [ServicioController::class, 'index']);
+$router->get('/servicios/crear', [ServicioController::class, 'crear']);
+$router->post('/servicios/crear', [ServicioController::class, 'crear']);
+$router->get('/servicios/actualizar', [ServicioController::class, 'actualizar']);
+$router->post('/servicios/actualizar', [ServicioController::class, 'actualizar']);
+$router->post('/servicios/eliminar', [ServicioController::class, 'eliminar']);
+
+
 
 
 
